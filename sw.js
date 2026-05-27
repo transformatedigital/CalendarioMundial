061610c80878c8ee04978d45fd6dea6116c7170b
cesar <cefi2012@gmail.com>
2026-05-27 17:45:52 +0900

    3 selectores de ubicacion independientes en el header

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service worker básico: cache-first para uso offline
-const CACHE = 'mundial-2026-v6';
+const CACHE = 'mundial-2026-v7';
 const ASSETS = ['./', './index.html', './data.js', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
 
 self.addEventListener('install', e => {
